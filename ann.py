58bce5df5bd220c93be56bc6ac3c54c8d3fb08d5
oussama <abz1999abz@gmail.com>
2021-01-27 19:04:22 +0100

    last commit

--- ann.py
+++ ann.py
@@ -31,3 +31,15 @@ classifier.summary()
 classifier.compile(optimizer = 'adam',loss='mean_squared_error')
 hist = classifier.fit(X_train,y_train,epochs=100,batch_size = 10,verbose=1,validation_split=0.2)
 
+plt.plot(hist.history['loss'])
+plt.plot(hist.history['val_loss'])
+plt.title('loss during training')
+plt.xlabel('epochs')
+plt.ylabel('loss')
+plt.legend(['trainin','validation'])
+
+y_pred = classifier.predict(X_test)
+a = 0
+for i in range(len(y_pred)):
+    a += abs(y_pred[i]-y_test[i])
+print("la diff moy est : "+str(a/len(y_pred)))
